e9d999d78ce23bea95981ccad2b2ef26a95df90c
Nick Palladinos <nick.palladinos@gmail.com>
2016-08-23 19:36:29 +0300

    Add "A simple Quine in F#"

---
+++ src/FSharpSnippets/A simple Quine.fsx
@@ -0,0 +1,4 @@
+﻿// A simple Quine in F# 
+
+let s : Printf.TextWriterFormat<_> = "let s : Printf.TextWriterFormat<_> = %c%s%c in
+printf s (char 34) (s.Value) (char 34)" in printf s (char 34) (s.Value) (char 34)
