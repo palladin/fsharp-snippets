ebe1683ce7cec5f6863fe9e2cd854bc69fc5471b
Nick Palladinos <nick.palladinos@gmail.com>
2016-08-23 19:03:00 +0300

    Add "A Lazy fixed-point combinator"

---
+++ src/FSharpSnippets/A Lazy fixed-point combinator.fsx
@@ -0,0 +1,12 @@
+﻿// x = f(x) encoded in F#
+
+let force (value : Lazy<_>) = value.Force()
+
+let fix f = let rec x = lazy (f x) in x
+
+// Examples
+let fac = fix (fun f x -> if x = 0 then 1 else x * force f (x - 1) )
+let nums = fix (fun v -> seq { yield 0; yield! Seq.map ((+) 1) (force v) }) 
+
+force fac 10 // 10! = 3628800
+Seq.take 10 (force nums) // seq [0; 1; 2; 3; ...]
