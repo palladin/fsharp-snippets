7e88b70e658bc0e9056e924cba004e8e5b143747
Nick Palladinos <nick.palladinos@gmail.com>
2016-08-23 18:45:01 +0300

    Add "Functional style Regex engine"

---
+++ src/FSharpSnippets/Functional style Regex engine.fsx
@@ -0,0 +1,14 @@
+﻿// Functional style Regex engine
+
+let char c (s : string) = seq { if s.Length > 0 && s.[0] = c then yield s.Substring(1) }
+
+let (=>) l r s = seq { for sl in l s do for sr in r sl -> sr }
+
+let (<|>) l r s = seq { yield! l s; yield! r s }
+
+let rec (<*>) e s = seq { yield s; yield! (e => (<*>) e) s }
+
+let (<+>) e = e => (<*>) e
+
+// example c(a|d)+r
+let pattern = char 'c' => (<+>) (char 'a' <|> char 'd') => char 'r'
