9697b0c605e7e756c439aece273372bc58d3f7d5
Nick Palladinos <nick.palladinos@gmail.com>
2016-08-23 18:48:23 +0300

    Add "Fixed-point ﬁnding function"

---
+++ src/FSharpSnippets/Fixed-point ﬁnding function.fsx
@@ -0,0 +1,11 @@
+﻿// We start with an initial value and then applying f repeatedly, until the value does not change anymore.
+
+let rec iterate f value = 
+  seq { yield value; 
+        yield! iterate f (f value) }
+
+let fixedPoint f initial = 
+    iterate f initial 
+    |> Seq.pairwise 
+    |> Seq.pick (fun (first, second) -> 
+        if first = second then Some first else None)
