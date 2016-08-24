2425c322c1fa802d017e999732bb16cf2c91ff80
Nick Palladinos <nick.palladinos@gmail.com>
2016-08-24 19:14:57 +0300

    Add "Y(n) Polyvariadic fixpoint"

---
+++ src/FSharpSnippets/Y(n) Polyvariadic fixpoint.fsx
@@ -0,0 +1,18 @@
+﻿// Encoding mutually-recursive functions with a Polyvariadic fixpoint combinator.
+
+let rec Y f x = f (Y f) x 
+
+let rec Y2 f1 f2 =
+    let f1' = Y (fun f1' -> f1 f1' (Y (fun f2' -> f2 f1' f2')))
+    let f2' = Y (fun f2' -> f2 (Y (fun f1' -> f1 f1' f2')) f2')
+    f1', f2'
+
+// Example
+let even, odd = 
+    Y2 (fun even odd x ->
+           x = 0 || odd (x-1))
+       (fun even odd x ->
+           x <> 0 && even (x-1))
+
+even 42 // true 
+odd 42 // false
