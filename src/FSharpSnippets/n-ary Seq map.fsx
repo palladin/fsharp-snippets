3b69ce1f1861afa11520cd9803b8f1eed608c6bf
Nick Palladinos <nick.palladinos@gmail.com>
2016-08-24 19:10:45 +0300

    Add "n-ary Seq.map"

---
+++ src/FSharpSnippets/n-ary Seq map.fsx
@@ -0,0 +1,8 @@
+﻿// A pattern for creating n-ary Seq.map functions.
+
+let (<*>) fs xs = Seq.map2 (fun f x -> f x) fs xs
+
+let map3 f xs bs cs = Seq.map2 f xs bs <*> cs
+let map4 f xs bs cs ds = map3 f xs bs cs <*> ds
+
+map4 (fun x b c d -> x + b + c + d) [1;2] [1;2] [1;2] [1;2] // [4; 8]
