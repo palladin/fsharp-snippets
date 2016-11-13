72c63e4832b71f5875c2bb33c67219f0fa6359e6
Nick Palladinos <nick.palladinos@gmail.com>
2016-11-13 20:57:46 +0200

    Add parser combinators (andthen, orelse, many).

--- src/FSharpSnippets/Staged Parser Combinators.fsx
+++ src/FSharpSnippets/Staged Parser Combinators.fsx
@@ -25,6 +25,17 @@ let lambda2 (f : Expr<'T> -> Expr<'S> -> Expr<'R>) : Expr<'T -> 'S -> 'R> =
     let var' = new Var(sprintf "__paramTemp_%d__" !counter, typeof<'S>)
     Expr.Cast<_>(Expr.Lambda(var, Expr.Lambda(var',  f (Expr.Cast<_>(Expr.Var var)) (Expr.Cast<_>(Expr.Var var')))))
 
+// <@ fun x y z -> (% <@ x @> ... <@ y @> ... <@ z @> ) @> ~ lambda (fun x y z -> x ... y ... z )
+let lambda3 (f : Expr<'T> -> Expr<'S> -> Expr<'K> -> Expr<'R>) : Expr<'T -> 'S -> 'K -> 'R> =
+    incr counter
+    let var = new Var(sprintf "__paramTemp_%d__" !counter, typeof<'T>)
+    incr counter
+    let var' = new Var(sprintf "__paramTemp_%d__" !counter, typeof<'S>)
+    incr counter
+    let var'' = new Var(sprintf "__paramTemp_%d__" !counter, typeof<'K>)
+    Expr.Cast<_>(Expr.Lambda(var, Expr.Lambda(var', Expr.Lambda(var'', f (Expr.Cast<_>(Expr.Var var)) (Expr.Cast<_>(Expr.Var var')) (Expr.Cast<_>(Expr.Var var''))))))
+
+
 type Parser<'T> = Expr<string> -> Expr<int> -> (Expr<'T> -> Expr<string> -> Expr<int> -> Expr<bool>) -> Expr<bool>
 
 
@@ -42,13 +53,23 @@ let pchar (matchChar : char) : Parser<char> =
 
 
 let (=>) (left : Parser<'T>) (right : Parser<'S>) : Parser<'T * 'S> = 
-    fun str index k -> failwith "oups"
+    fun str index k -> left str index (fun value str index -> right str index (fun value' str index -> k <@ (%value, %value') @> str index))
 
 let (<|>) (left : Parser<'T>) (right : Parser<'T>) : Parser<'T> = 
-    fun str index k -> failwith "oups"
+    fun str index k -> <@ let test = (% left str index (fun value  str index -> k value str index)) 
+                          if test then true
+                          else (% right str index (fun value  str index -> k value str index))  
+                       @>
 
-let (<*>) (parser : Parser<'T>) : Parser<'T []> = 
-    fun str index k -> failwith "oups"
+let (<*>) (parser : Parser<'T>) : Parser<'T list> = 
+    fun str index k -> 
+        <@  let rec loop (index : int) (acc : 'T list) =
+                let test = (% lambda3 (fun loop index acc -> parser str index (fun value str index -> <@ (%loop) %index (%value :: %acc) @>))) loop index acc
+                if not test then
+                    (% lambda2 (fun index acc -> k acc str index)) index acc
+                else true
+            loop %index []
+        @>
 
 let compileParser (parser : Parser<'T>) : string -> 'T option = 
     let f = QuotationCompiler.ToFunc(lambda (fun (str : Expr<string>) -> 
@@ -65,7 +86,16 @@ let f = pchar 'a' |> compileParser
 f "abc" // Some 'a'
 f "bac" // None
 
+let g = (pchar 'a' => pchar 'b') |> compileParser
+g "abc" // Some ('a', 'b')
+g "bac" // None
 
+let h = (pchar 'a' <|> pchar 'b') |> compileParser
+h "abc" // Some 'a'
+h "bac" // Some 'b'
+h "cab" // None
 
-
-
+let k = ((<*>) (pchar 'a')) |> compileParser
+k "abc" // Some ['a']
+k "aabc" // Some ['a'; 'a']
+k "bac" // None
